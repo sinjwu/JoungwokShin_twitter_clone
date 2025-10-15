36aa66575cdb6704cbb2576a84625995cc77b698
sinjwu <harbirdouk@gmail.com>
2025-10-16 01:55:30 +0900

    first commit

---
+++ backend/src/main/java/com/example/backend/controller/AuthController.java
@@ -0,0 +1,12 @@
+package com.example.backend.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final AuthService authService;
+}
